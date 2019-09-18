1609720c6c47a2aaadbbad5eb205de9e608bc4b0
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-09-18 19:36:59 +0900

    tableview name ok....

---
+++ ContinuationIsPower/Model/object.swift
@@ -0,0 +1,67 @@
+//
+//  object.swift
+//  ContinuationIsPower
+//
+//  Created by 笹倉一也 on 2019/09/18.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import Foundation
+
+
+class Set {
+    
+    
+    var setName:String = ""
+
+    var kg1:String = ""
+    var rep1:String = ""
+    
+    var kg2:String = ""
+    var rep2:String = ""
+   
+    var kg3:String = ""
+    var rep3:String = ""
+    
+    var kg4:String = ""
+    var rep4:String = ""
+    
+    var kg5:String = ""
+    var rep5:String = ""
+    
+    var alphaKg:String = ""
+    var alphaRep:String = ""
+    
+    init(setname: String,
+         kg1:String, rep1:String,
+         kg2:String, rep2:String,
+         kg3:String, rep3:String,
+         kg4:String, rep4:String,
+         kg5:String, rep5:String,
+         alphaKg:String, alphaRep:String) {
+        
+         self.setName = setname
+        
+         self.kg1 = kg1
+         self.rep1 = rep1
+        
+         self.kg2 = kg2
+         self.rep2 = rep2
+        
+         self.kg3 = kg3
+         self.rep3 = rep3
+        
+         self.kg4 = kg4
+         self.rep4 = rep4
+        
+         self.kg5 = kg5
+         self.rep5 = rep5
+        
+         self.alphaKg = alphaKg
+         self.alphaRep = alphaRep
+        
+        
+        
+    }
+    
+}
--- ContinuationIsPower/SetMenu/mySet.swift
+++ ContinuationIsPower/SetMenu/mySet.swift
@@ -14,19 +14,85 @@ import Firebase
 class mySet: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
-    let item = ["kdkl", "mdvd;l"]
+    var setItem = [Set]()
 
+    @IBOutlet weak var tableview: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let db = Firestore.firestore()
+        
+//        let user = Auth.auth().currentUser
+        
+        let ref = db.collection("myset")
+        
+        ref.addSnapshotListener(includeMetadataChanges: true){ (document, error) in
+            
+            
+            
+            guard let value = document else {
+                print("snapShot is nil")
+                return
+            }
+            
+            
+            value.documentChanges.forEach{ postdiff in
+                
+                if postdiff.type == .added {
+                    
+                    
+                    
+                    let chatDataOp = postdiff.document.data() as? Dictionary<String, Any>
+                    
+                    
+                    guard let chatData = chatDataOp else {
+                        return
+                    }
+                    
+                    
+                    let setname = chatData["セットネーム"] as? String
+                    
+                    let kg1 = chatData["1セットKG"] as? String
+                    let rep1 = chatData["1セットRep"] as? String
+                    
+                    let kg2 = chatData["2セットKG"] as? String
+                    let rep2 = chatData["2セットRep"] as? String
+                    
+                    let kg3 = chatData["3セットKG"] as? String
+                    let rep3 = chatData["3セットRep"] as? String
+                    
+                    let kg4 = chatData["4セットKG"] as? String
+                    let rep4 = chatData["4セットRep"] as? String
+                    
+                    let kg5 = chatData["5セットKG"] as? String
+                    let rep5 = chatData["5セットRep"] as? String
+                    
+                    let alphakg = chatData["プラスアルファKG"] as? String
+                    let alpharep = chatData["プラスアルフアRep"] as? String
+                    
+                    
+                    
+                    let newSourse = Set(setname: setname ?? "", kg1: kg1 ?? "", rep1: rep1 ?? "", kg2: kg2 ?? "", rep2: rep2 ?? "", kg3: kg3 ?? "", rep3: rep3 ?? "", kg4: kg4 ?? "", rep4: rep4 ?? "", kg5: kg5 ?? "", rep5: rep5 ?? "", alphaKg: alphakg ?? "", alphaRep: alpharep ?? "")
+                    
+                    self.setItem.append(newSourse)
+                    
+                    self.tableview.reloadData()
+                    
+                }
 
+            }
+            
+        }
+    
     }
 
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return item.count
+        return setItem.count
     }
     
     
@@ -34,7 +100,7 @@ class mySet: UIViewController, UITableViewDelegate, UITableViewDataSource {
        
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
        
-        cell.textLabel!.text = item[indexPath.row]
+        cell.textLabel!.text = setItem[indexPath.row].setName
         
         return cell
         
