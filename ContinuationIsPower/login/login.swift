bd15999c07778aa19b8b3aa4228664001d33acf9
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-09-25 13:24:20 +0900

    login file class ok.......

---
+++ ContinuationIsPower/login/login.swift
@@ -0,0 +1,22 @@
+//
+//  login.swift
+//  ContinuationIsPower
+//
+//  Created by 笹倉一也 on 2019/09/25.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
---
+++ ContinuationIsPower/login/signup.swift
@@ -0,0 +1,21 @@
+//
+//  signup.swift
+//  ContinuationIsPower
+//
+//  Created by 笹倉一也 on 2019/09/25.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class signup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
