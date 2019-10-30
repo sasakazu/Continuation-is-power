2c44b188d12933248e1fe68b968e892743e8fd2c
笹倉一也 <kazuya.sasakura@gmail.com>
2019-10-30 13:24:10 +0900

    static table base ok.....

---
+++ ContinuationIsPower/More/moreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  moreViewController.swift
+//  ContinuationIsPower
+//
+//  Created by 笹倉一也 on 2019/10/30.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class moreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
