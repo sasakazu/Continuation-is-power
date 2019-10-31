a6f1e8de287cc5383efdd5b8392318250f397b53
笹倉一也 <kazuya.sasakura@gmail.com>
2019-10-31 13:39:13 +0900

    little changes....

--- ContinuationIsPower/More/moreTableView.swift
+++ ContinuationIsPower/More/moreTableView.swift
@@ -33,7 +33,7 @@ class moreTableView: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 4
     }
 
 
