9131b8b999ca14ca6b611ec1725d7e010a0a973f
笹倉一也 <kazuya.sasakura@gmail.com>
2019-11-06 13:20:25 +0900

    login sign up next ........

---
+++ ContinuationIsPower/More/timeWatch.swift
@@ -0,0 +1,42 @@
+//
+//  timeWatch.swift
+//  ContinuationIsPower
+//
+//  Created by 笹倉一也 on 2019/11/06.
+//  Copyright © 2019 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class timeWatch: UIViewController {
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    @IBAction func startBtn(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+
+
+}
